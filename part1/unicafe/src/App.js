177261cd309736cef7b2c76b3cf9062b65b414c3
robertmarinescu <robert.marinescu93@gmail.com>
2021-05-07 15:29:17 +0300

    move statistics to an array of objects

--- part1/unicafe/src/App.js
+++ part1/unicafe/src/App.js
@@ -1,13 +1,21 @@
-import React, {useState} from "react";
+import React, { useState } from "react";
 
-const Title = ({title}) => <h1>{title}</h1> 
-const Button = ({onClick, text}) => <button onClick={onClick}>{text}</button>
-const Statistics = (props) => <tr><td>{props.text}</td> <td>{props.results}</td></tr>
+const Title = ({ title }) => <h1>{title}</h1>;
+const Button = ({ onClick, text }) => <button onClick={onClick}>{text}</button>;
+const Statistics = (props) => (
+  <tr>
+    <td>{props.text}</td>
+    <td>{props.results}</td>
+  </tr>
+);
 
-const NoFeedback = () => <tr><td>No feedback given</td></tr>
+const NoFeedback = () => (
+  <tr>
+    <td>No feedback given</td>
+  </tr>
+);
 
 const App = () => {
-  
   const [good, setGood] = useState(0);
   const [neutral, setNeutral] = useState(0);
   const [bad, setBad] = useState(0);
@@ -16,29 +24,29 @@ const App = () => {
   const handleGoodClick = () => {
     setGood(good + 1);
     setAll(all + 1);
-  }
+  };
   const handleNeutralClick = () => {
     setNeutral(neutral + 1);
     setAll(all + 1);
-  }
+  };
   const handleBadClick = () => {
     setBad(bad + 1);
     setAll(all + 1);
-  }
+  };
 
   const average = (good - bad) / all;
   const positive = (good * 100) / all;
 
-  const statistics = [
-    <Statistics text="good" results={good}/>,
-    <Statistics text="neutral" results={neutral}/>,
-    <Statistics text="bad" results={bad}/>,
-    <Statistics text="all" results={all}/>,
-    <Statistics text="average" results={average > 0 ? average : 0}/>,
-    <Statistics text="positive" results={positive > 0 ? positive : 0}/>
-  ]
+  const statisticsArray = [
+    { text: "good", results: good },
+    { text: "neutral", results: neutral },
+    { text: "bad", results: bad },
+    { text: "all", results: all },
+    { text: "average", results: average > 0 ? average : 0 },
+    { text: "positive", results: `${positive > 0 ? positive : 0} %` },
+  ];
 
-  return(
+  return (
     <div>
       <Title title="give feedback"></Title>
       <Button onClick={handleGoodClick} text="good"></Button>
@@ -47,14 +55,22 @@ const App = () => {
 
       <Title title="statistics"></Title>
       <table>
-      {
-        !all 
-          ? <NoFeedback/>
-          : statistics.map(comp => comp)
-      }
+        <tbody>
+          {!all ? (
+            <NoFeedback />
+          ) : (
+            statisticsArray.map((value, index) => (
+              <Statistics
+                key={index}
+                text={value.text}
+                results={value.results}
+              />
+            ))
+          )}
+        </tbody>
       </table>
     </div>
-  )
-}
+  );
+};
 
 export default App;
