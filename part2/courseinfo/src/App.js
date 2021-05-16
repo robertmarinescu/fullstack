872bb5ad42bad2110819c0a9d57d4ecdeb2fa7a1
robertmarinescu <robert.marinescu93@gmail.com>
2021-05-16 18:13:46 +0300

    Add sum of the exercises component

--- part2/courseinfo/src/App.js
+++ part2/courseinfo/src/App.js
@@ -1,25 +1,73 @@
-// import logo from "./logo.svg";
-// import "./App.css";
+import React from "react";
 
-// function App() {
-//   return (
-//     <div className="App">
-//       <header className="App-header">
-//         <img src={logo} className="App-logo" alt="logo" />
-//         <p>
-//           Edit <code>src/App.js</code> and save to reload.
-//         </p>
-//         <a
-//           className="App-link"
-//           href="https://reactjs.org"
-//           target="_blank"
-//           rel="noopener noreferrer"
-//         >
-//           Learn React
-//         </a>
-//       </header>
-//     </div>
-//   );
-// }
+const Header = ({ course }) => {
+  return <h1>{course.name}</h1>;
+};
 
-// export default App;
+const Total = ({ course }) => {
+  const total = course.parts.reduce((sum, numberOfExercises) => {
+    return sum + numberOfExercises.exercises;
+  }, 0);
+  return <p style={{ fontWeight: "bold" }}>total of {total} exercises</p>;
+};
+
+const Part = ({ courseObject }) => {
+  return (
+    <p>
+      {courseObject.name} {courseObject.exercises}
+    </p>
+  );
+};
+
+const Content = ({ courses }) => {
+  return (
+    <div>
+      {courses.parts.map((course) => (
+        <Part key={course.id} courseObject={course}></Part>
+      ))}
+    </div>
+  );
+};
+
+const Course = ({ course }) => {
+  return (
+    <div>
+      <Header course={course}></Header>
+      <Content courses={course}></Content>
+      <Total course={course}></Total>
+    </div>
+  );
+};
+
+const App = () => {
+  const course = {
+    id: 1,
+    name: "Half Stack application development",
+    parts: [
+      {
+        name: "Fundamentals of React",
+        exercises: 10,
+        id: 1,
+      },
+      {
+        name: "Using props to pass data",
+        exercises: 7,
+        id: 2,
+      },
+      {
+        name: "State of a component",
+        exercises: 14,
+        id: 3,
+      },
+      {
+        name: "Redux",
+        exercises: 11,
+        id: 4,
+      }
+    ],
+  };
+
+  return <Course course={course}></Course>;
+};
+
+export default App;
