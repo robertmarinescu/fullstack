9e8ad8a88ed6255af7e16ce031b09e6c4223a3bb
robertmarinescu <robert.marinescu93@gmail.com>
2021-07-14 20:08:55 +0300

    implement the functionality for displaying a single resource

--- part3/phonebook-backend/index.js
+++ part3/phonebook-backend/index.js
@@ -36,9 +36,18 @@ app.get("/api/persons", (req, res) => {
 
 app.get("/info", (req, res) => {
   res.send(
-    `<p>Phonebook has info for ${persons.length} people</p>
-      <p>${new Date()}</p>`
+    `<div>
+        <p>Phonebook has info for ${persons.length} people</p>
+        <p>${new Date()}</p>
+    </div>`
   );
+});
+
+app.get("/api/persons/:id", (req, res) => {
+  const id = Number(req.params.id);
+  const person = persons.find((person) => person.id === id);
+
+  return person ? res.json(person) : res.status(404).end();
 });
 
 const PORT = 3001;
