60ae236424d2b498610739c8d7b372d84dd4a41b
robertmarinescu <robert.marinescu93@gmail.com>
2021-07-16 15:31:51 +0300

    Add morgan middleware to application for logging. Configure morgan so that it also shows the data sent in HTTP POST requests

--- part3/phonebook-backend/index.js
+++ part3/phonebook-backend/index.js
@@ -1,17 +1,18 @@
 const express = require("express");
+const morgan = require("morgan");
 const app = express();
 
 app.use(express.json());
 
-const requestLogger = (request, response, next) => {
-  console.log("Method:", request.method);
-  console.log("Path:  ", request.path);
-  console.log("Body:  ", request.body);
-  console.log("---");
-  next();
-};
+morgan.token("details", (req, res) => {
+  return JSON.stringify(req.body);
+});
 
-app.use(requestLogger);
+app.use(
+  morgan(
+    ":method :url :status :res[content-length] - :response-time ms :details"
+  )
+);
 
 let persons = [
   {
