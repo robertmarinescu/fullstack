3a3077579970461a75500017a018c0c13c79a8b0
robertmarinescu <robert.marinescu93@gmail.com>
2021-07-19 17:22:06 +0300

    Upload phonebook-backend to heroku

--- part3/phonebook-backend/index.js
+++ part3/phonebook-backend/index.js
@@ -110,7 +110,7 @@ app.post("/api/persons", (req, res) => {
   return res.json(person);
 });
 
-const PORT = 3001;
+const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
