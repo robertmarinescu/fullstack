c4fc28f19caa9b07139008910f1935098c6dc894
robertmarinescu <robert.marinescu93@gmail.com>
2022-01-03 17:27:40 +0200

    allow users to create blogs directly from the frontend

--- part5/bloglist-frontend/src/App.js
+++ part5/bloglist-frontend/src/App.js
@@ -11,6 +11,10 @@ const App = () => {
   const [password, setPassword] = useState('')
   const [user, setUser] = useState(null)
 
+  const [title, setTitle] = useState('')
+  const [author, setAuthor] = useState('')
+  const [url, setUrl] = useState('')
+
   useEffect(() => {
     const loggedOnUserJson = window.localStorage.getItem('loggedOnAppBlogList')
     if(loggedOnUserJson){
@@ -51,6 +55,15 @@ const App = () => {
     setUser(null)
   }
 
+  const handleCreateBlog = async (event) => {
+    event.preventDefault()
+    await blogService.create({title, author, url})
+    setTitle('')
+    setAuthor('')
+    setUrl('')
+    fetchBlogs()
+  }
+
   const loginForm = () => {
     if(user === null) {
       return (
@@ -84,6 +97,37 @@ const App = () => {
       <div>
         <h2>blogs</h2>
         <p>{user.name} logged-in <button onClick={handleLogOut}>logout</button></p>
+        <h2>create new blog</h2>
+        <form onSubmit={handleCreateBlog}>
+          <div>
+             title:
+               <input
+                type="text"
+                value={title}
+                name="title"
+                onChange={({ target }) => setTitle(target.value)}
+              />
+          </div>
+          <div>
+             author:
+               <input
+                type="text"
+                value={author}
+                name="author"
+                onChange={({ target }) => setAuthor(target.value)}
+              />
+          </div>
+          <div>
+             url:
+               <input
+                type="text"
+                value={url}
+                name="url"
+                onChange={({ target }) => setUrl(target.value)}
+              />
+          </div>
+          <button type='submit'>create</button>
+        </form>
         <div>
           {blogs.map(blog =>
             <Blog key={blog.id} blog={blog} />
