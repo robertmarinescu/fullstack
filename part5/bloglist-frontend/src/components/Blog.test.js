1683a575a74cebcdf3777830a5f6efe9963f42ca
robertmarinescu <robert.marinescu93@gmail.com>
2022-01-08 18:46:40 +0200

    check that the blog's url and number of likes are shown when the 'show' button is clicked

--- part5/bloglist-frontend/src/components/Blog.test.js
+++ part5/bloglist-frontend/src/components/Blog.test.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import '@testing-library/jest-dom/extend-expect'
-import { render } from '@testing-library/react'
+import { render, fireEvent } from '@testing-library/react'
 import Blog from './Blog'
 
 describe('<Blog />', () => {
@@ -30,6 +30,34 @@ describe('<Blog />', () => {
 
     expect(title).toHaveTextContent('Test Blog 1')
     expect(author).toHaveTextContent('John Smith')
+    expect(url).toHaveTextContent('testblog.com')
+    expect(likes).toHaveTextContent('10')
+  })
+
+  test('check that the blog\'s url and number of likes are shown when the \'show\' button is clicked', () => {
+    const blog = {
+      title: 'Test Blog 1',
+      author: 'John Smith',
+      url: 'testblog.com',
+      likes: 10,
+      user: {
+        name: 'John Smith',
+        username: 'johnsmith'
+      }
+    }
+
+    const component = render(
+      <Blog blog={blog}/>
+    )
+
+    const button = component.getByText('view')
+    fireEvent.click(button)
+
+    const defaultHiddenContent = component.container.querySelector('.hiddenContent')
+    const url = component.container.querySelector('.blogUrl')
+    const likes = component.container.querySelector('.blogLikes')
+
+    expect(defaultHiddenContent).not.toHaveStyle('display: none')
     expect(url).toHaveTextContent('testblog.com')
     expect(likes).toHaveTextContent('10')
   })
