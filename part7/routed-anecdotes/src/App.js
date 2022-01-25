cbc024ee46c1e751a34e98d80c18966e0cdf4e44
robertmarinescu <robert.marinescu93@gmail.com>
2022-01-25 10:55:37 +0200

    add a button to clear all of the input fields

--- part7/routed-anecdotes/src/App.js
+++ part7/routed-anecdotes/src/App.js
@@ -78,23 +78,30 @@ const CreateNew = (props) => {
     history.push('/')
   }
 
+  const reset = () => {
+    content.reset()
+    author.reset()
+    info.reset()
+  }
+
   return (
     <div>
       <h2>create a new anecdote</h2>
       <form onSubmit={handleSubmit}>
         <div>
           content
-          <input {...content} />
+          <input type={content.type} value={content.value} onChange={content.onChange}/>
         </div>
         <div>
           author
-          <input {...author} />
+          <input type={author.type} value={author.value} onChange={author.onChange} />
         </div>
         <div>
           url for more info
-          <input {...info} />
+          <input type={info.type} value={info.value} onChange={info.onChange} />
         </div>
         <button>create</button>
+        <button type='button' onClick={reset}>reset</button>
       </form>
     </div>
   )
