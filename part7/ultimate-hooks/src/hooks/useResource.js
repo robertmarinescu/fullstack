a6d51fe9ff41be80efe8fab914641412d532a0b7
robertmarinescu <robert.marinescu93@gmail.com>
2022-01-25 19:29:08 +0200

    implement useResource hook

---
+++ part7/ultimate-hooks/src/hooks/useResource.js
@@ -0,0 +1,25 @@
+import { useState } from 'react'
+import axios from 'axios'
+
+export const useResource = (baseUrl) => {
+  const [resources, setResources] = useState([])
+
+  const getAll = async () => {
+    const response = await axios.get(baseUrl)
+    setResources(response.data)
+  }
+
+  const create = async (resource) => {
+    const response = await axios.post(baseUrl, resource)
+    setResources(resources.concat(response.data))
+  }
+
+  const service = {
+    create,
+    getAll
+  }
+
+  return [
+    resources, service
+  ]
+}
